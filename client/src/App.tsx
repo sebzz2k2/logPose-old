fde4c8e0323152c521c59a925f94874e4dcac25b
Sebin CM <sebinsebzz2002@gmail.com>
2023-11-22 20:16:15 +0530

    chore: add packages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,80 @@
-import "./App.css";
+// import Button from "@/components/Button";
+// import { Toaster } from "@/components/Toast/Toaster";
+// import Chip from "@/components/Chip";
+// import { Modal, ModalBody, ModalHeader } from "./components/Modal";
+// import { useState } from "react";
+// import {
+//   Table,
+//   TableBody,
+//   TableCaption,
+//   TableCell,
+//   TableHead,
+//   TableHeader,
+//   TableRow,
+// } from "@/components/Table";
+import { DataTable } from "./components/DataTable";
+import { Payment, columns } from "./components/Col";
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <head />
+//       <body>
+//         <main>{children}</main>
+//         <Toaster />
+//       </body>
+//     </html>
+//   );
+// }
+
+// import Input from "@/components/Input";
+// import {
+//   Select,
+//   SelectContent,
+//   SelectGroup,
+//   SelectItem,
+//   SelectLabel,
+//   SelectTrigger,
+//   SelectValue,
+// } from "@/components/Select";
+
+// import Card from "@/components/Cards";
+// import { useToast } from "./components/Toast/useToast";
+// import { ToastAction } from "./components/Toast/Toast";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const data: Payment[] = [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "72852f",
+      amount: 100,
+      status: "pending",
+      email: "wsd@ja.com",
+    },
+    {
+      id: "728e52f",
+      amount: 100,
+      status: "pending",
+      email: "sas@jnlkj",
+    },
+    // ...
+  ];
+  // const { toast } = useToast();
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // const openModal = () => {
+  //   setIsModalOpen(true);
+  // };
+
+  // const closeModal = () => {
+  //   setIsModalOpen(false);
+  // };
+  return <DataTable columns={columns} data={data} />;
 }
 
 export default App;
---
+++ client/src/components/Col.tsx
@@ -0,0 +1,82 @@
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import { ArrowUpDown } from "lucide-react";
+
+import {
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+} from "@/components/Dropdown";
+
+import Button from "@/components/Button";
+
+export type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button>
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
+  },
+];
