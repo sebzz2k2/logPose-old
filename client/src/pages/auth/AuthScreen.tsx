df7f7277cd1517cf4a14b07e7d37c17773ddbb81
Sebin CM <sebinsebzz2002@gmail.com>
2023-11-27 00:41:16 +0530

    style(auth): add auth screen template

--- client/src/pages/auth/AuthScreen.tsx
+++ client/src/pages/auth/AuthScreen.tsx
@@ -6,13 +6,15 @@ export default function AuthScreen({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex bg-black h-screen  w-screen justify-between">
+    <div className="flex bg-black h-screen  w-screen justify-between  ">
       <div className="left w-1/2 bg-primary-500"></div>
       <div className="right bg-[#161616] w-1/2">
         <div className="header w-full text-end p-6">
           <Logo />
         </div>
-        {children}
+        <div className="w-5/6 flex items-center  justify-center">
+          {children}
+        </div>
       </div>
     </div>
   );
