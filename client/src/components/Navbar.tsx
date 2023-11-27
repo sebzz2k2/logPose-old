a2214f7c1b4792280d7c21889f76926e37a6f84f
Sebin CM <sebinsebzz2002@gmail.com>
2023-11-28 00:44:32 +0530

    style: add navbar

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Logo from "./Logo";
+import { FaUserLarge } from "react-icons/fa6";
+
+const Navbar = () => {
+  const userName = "sebin";
+  return (
+    <nav className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <Logo />
+      <div className="flex flex-row gap-2 items-center">
+        <div className="font-normal text-white text-lg">
+          Hello, {userName} !
+        </div>
+        <div className="border-2 rounded-3xl p-2">
+          <FaUserLarge className="text-white text-xl" />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
