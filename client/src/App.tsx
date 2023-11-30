f8ffa1201df2bf2b54e60142ffac19c60bd63309
Sebin CM <sebinsebzz2002@gmail.com>
2023-11-30 21:01:16 +0530

    chore: cleanup assets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,15 @@ import NotFound from "./pages/misc/NotFound";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import { ForgotPassword } from "./pages/auth/ForgotPassword";
+import Home from "./pages/home";
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="auth">
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="forgot-password" element={<ForgotPassword />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
