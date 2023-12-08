b65d6dd0ad2315e6e39e1f2ff8d90728add1d104
Sebin CM <sebinsebzz2002@gmail.com>
2023-12-08 23:43:36 +0530

    chore: modularization

---
+++ client/src/components/index.ts
@@ -0,0 +1,9 @@
+import Button from "./Button";
+import Card from "./Cards";
+import Input from "./Input";
+import Logo from "./Logo";
+import { Modal } from "./Modal";
+import Navbar from "./Navbar";
+import { Select } from "./Select";
+
+export { Button, Card, Input, Logo, Modal, Navbar, Select };
