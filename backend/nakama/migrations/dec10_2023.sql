0916f5101d6e9082279c709e561301db766a301a
Sebin CM <sebinsebzz2002@gmail.com>
2023-12-10 22:20:44 +0530

    chore: update migration sql

--- backend/nakama/migrations/dec10_2023.sql
+++ backend/nakama/migrations/dec10_2023.sql
@@ -2,16 +2,19 @@ CREATE TABLE IF NOT EXISTS Users (
     id CHAR(36) NOT NULL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Roles (
+CREATE INDEX email_index ON Users(email);
+
+
+CREATE TABLE IF NOT EXISTS Roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     role_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE UserRoles (
+CREATE TABLE IF NOT EXISTS UserRoles (
     user_id CHAR(36),
     role_id INT,
     PRIMARY KEY (user_id, role_id)
